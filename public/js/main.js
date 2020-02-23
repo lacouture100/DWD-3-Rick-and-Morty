7358c1810a948d30bcafb409aa769b4412dc3dbc
Alvaro Lacouture <lacouture100@gmail.com>
2020-02-23 01:06:11 -0500

    Added container borders

--- public/js/main.js
+++ public/js/main.js
@@ -14,7 +14,7 @@ function bindButton() {
   const url3 = "https://trefle.io/api/plants/116138?token=NGxtMVFYbjdNV2JtTzQzK0xiQ2dTZz09";
   let contentPgs = 0;
   let charCnt = 0;
-  let pageNum=0;
+  let pageNum = 0;
 
   let textSize = '100%';
   let labelSize = '120%';
@@ -28,20 +28,20 @@ function bindButton() {
 
 
   var charContainerSelector = document.querySelector(".characterCard--normal");
-  if (charContainerSelector != null){
-  console.log("Container detected!");
-  charContainerSelector.addEventListener('mouseover', function () {
-    console.log("focus!");
-    if (charContainerSelector.contains('characterCard--normal')) {
-      charContainerSelector.remove('characterCard--normal')
-      charContainerSelector.add('characterCard--focus')
-      
-    } else {
-      charContainerSelector.remove('characterCard--focus')
-      charContainerSelector.add('characterCard--normal')
+  if (charContainerSelector != null) {
+    console.log("Container detected!");
+    charContainerSelector.addEventListener('mouseover', function () {
+      console.log("focus!");
+      if (charContainerSelector.contains('characterCard--normal')) {
+        charContainerSelector.remove('characterCard--normal')
+        charContainerSelector.add('characterCard--focus')
 
-    }
-  });
+      } else {
+        charContainerSelector.remove('characterCard--focus')
+        charContainerSelector.add('characterCard--normal')
+
+      }
+    });
   }
 
 
@@ -62,95 +62,100 @@ function bindButton() {
 
   // to use await, the function must be marked as async
   button.onclick = async () => {
-    pageNum ++;
+    pageNum++;
 
 
     //Loop through every available page and grab every result
     let contentPgs = 2;
     //for (var pageNum = 1; pageNum < contentPgs; pageNum++) {
-      fetch('https://rickandmortyapi.com/api/character/?page=' + pageNum)
+    fetch('https://rickandmortyapi.com/api/character/?page=' + pageNum)
 
-        //Fetch page number pagNum and parse as JSON
-        .then(response => response.json())
-        .then(result => {
-          //Grab every result element (R&M character) from the results array
-          //console.log(result)
-          result.results.forEach(element => {
+      //Fetch page number pagNum and parse as JSON
+      .then(response => response.json())
+      .then(result => {
+        //Grab every result element (R&M character) from the results array
+        //console.log(result)
+        result.results.forEach(element => {
 
-            //Make a new <div> container with each character's info
-            const charContainer = document.createElement("div");
-            charContainer.setAttribute("class", "characterCard--normal");
-            mainContainer.appendChild(charContainer);
+          //Make a new <div> container with each character's info
+          const charContainer = document.createElement("div");
+          charContainer.setAttribute("class", "characterCard--normal");
+          mainContainer.appendChild(charContainer);
 
-            //Make a new <img> element with each character's image
-            const img = document.createElement("img");
-            img.src = element.image;
-            img.setAttribute("class", "character__image");
-            charContainer.appendChild(img);
+          //Make a new <img> element with each character's image
+          const img = document.createElement("img");
+          img.src = element.image;
+          img.setAttribute("class", "character__image");
+          charContainer.appendChild(img);
 
-            //Make a new <p> element with each character's name
-            const charName = document.createElement("p");
-            const nameLabel = document.createElement("p");
-            nameLabel.textContent = `Name:`;
-            charName.textContent = `${element.name}`;
-            nameLabel.setAttribute("class", "character__name");
-            nameLabel.style.fontSize = textSize;
-            charName.setAttribute("class", "character__name");
-            charName.style.fontSize = labelSize;
-            charContainer.appendChild(nameLabel);
-            charContainer.appendChild(charName);
+          //Make a new <img> element with each character's image
+          const charText = document.createElement("div");
+          charText.setAttribute("class", "characterText");
+          charContainer.appendChild(charText);
 
-            //Make a new <p> element with each character's status
-            const charStatus = document.createElement("p");
-            const statusLabel = document.createElement("p");
-            statusLabel.textContent = `Status:`;
-            charStatus.textContent = `${element.status}`;
-            statusLabel.setAttribute("class", "character__status");
-            statusLabel.style.fontSize = textSize;
-            charStatus.setAttribute("class", "character__status");
-            charStatus.style.fontSize = labelSize;
-            charContainer.appendChild(statusLabel);
-            charContainer.appendChild(charStatus);
+          //Make a new <p> element with each character's name
+          const charName = document.createElement("p");
+          const nameLabel = document.createElement("p");
+          nameLabel.textContent = `Name:`;
+          charName.textContent = `${element.name}`;
+          nameLabel.setAttribute("class", "character__name");
+          nameLabel.style.fontSize = textSize;
+          charName.setAttribute("class", "character__name");
+          charName.style.fontSize = labelSize;
+          charText.appendChild(nameLabel);
+          charText.appendChild(charName);
 
-            //Make a new <p> element with each character's species
-            const charSpecies = document.createElement("p");
-            const speciesLabel = document.createElement("p");
-            speciesLabel.textContent = `Species:`;
-            charSpecies.textContent = `${element.species}`;
-            speciesLabel.setAttribute("class", "character__species");
-            speciesLabel.style.fontSize = textSize;
-            charSpecies.setAttribute("class", "character__species");
-            charSpecies.style.fontSize = labelSize;
-            charContainer.appendChild(speciesLabel);
-            charContainer.appendChild(charSpecies);
+          //Make a new <p> element with each character's status
+          const charStatus = document.createElement("p");
+          const statusLabel = document.createElement("p");
+          statusLabel.textContent = `Status:`;
+          charStatus.textContent = `${element.status}`;
+          statusLabel.setAttribute("class", "character__status");
+          statusLabel.style.fontSize = textSize;
+          charStatus.setAttribute("class", "character__status");
+          charStatus.style.fontSize = labelSize;
+          charText.appendChild(statusLabel);
+          charText.appendChild(charStatus);
 
-            //Make a new <p> element with each character's origin
-            const charOrigin = document.createElement("p");
-            const originLabel = document.createElement("p");
-            originLabel.textContent = `Origin:`;
-            charOrigin.textContent = `${element.origin.name}`;
-            originLabel.setAttribute("class", "character__origin");
-            originLabel.style.fontSize = textSize;
-            charOrigin.setAttribute("class", "character__origin");
-            charOrigin.style.fontSize = labelSize;
-            charContainer.appendChild(originLabel);
-            charContainer.appendChild(charOrigin);
+          //Make a new <p> element with each character's species
+          const charSpecies = document.createElement("p");
+          const speciesLabel = document.createElement("p");
+          speciesLabel.textContent = `Species:`;
+          charSpecies.textContent = `${element.species}`;
+          speciesLabel.setAttribute("class", "character__species");
+          speciesLabel.style.fontSize = textSize;
+          charSpecies.setAttribute("class", "character__species");
+          charSpecies.style.fontSize = labelSize;
+          charText.appendChild(speciesLabel);
+          charText.appendChild(charSpecies);
 
-            //Make a new <p> element with each character's location
-            const charLoc = document.createElement("p");
-            const locLabel = document.createElement("p");
-            locLabel.textContent = `Location:`;
-            charLoc.textContent = `${element.location.name}`;
-            locLabel.setAttribute("class", "character__location");
-            locLabel.style.fontSize = textSize;
-            charLoc.setAttribute("class", "character__location");
-            charLoc.style.fontSize = labelSize;
-            charContainer.appendChild(locLabel);
-            charContainer.appendChild(charLoc);
+          //Make a new <p> element with each character's origin
+          const charOrigin = document.createElement("p");
+          const originLabel = document.createElement("p");
+          originLabel.textContent = `Origin:`;
+          charOrigin.textContent = `${element.origin.name}`;
+          originLabel.setAttribute("class", "character__origin");
+          originLabel.style.fontSize = textSize;
+          charOrigin.setAttribute("class", "character__origin");
+          charOrigin.style.fontSize = labelSize;
+          charText.appendChild(originLabel);
+          charText.appendChild(charOrigin);
 
-          });
+          //Make a new <p> element with each character's location
+          const charLoc = document.createElement("p");
+          const locLabel = document.createElement("p");
+          locLabel.textContent = `Location:`;
+          charLoc.textContent = `${element.location.name}`;
+          locLabel.setAttribute("class", "character__location");
+          locLabel.style.fontSize = textSize;
+          charLoc.setAttribute("class", "character__location");
+          charLoc.style.fontSize = labelSize;
+          charText.appendChild(locLabel);
+          charText.appendChild(charLoc);
 
-        })
-    }
+        });
+
+      })
+  }
   //}
 }
